ef6cb9ca236b23c09152c2b320253eb26153dcff
Jayant Patidar <jpatidar8740@gmail.com>
2019-10-23 14:31:07 +0530

    Added Passport

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 var express = require("express");
-var app = express();
+var session = require("express-session");
+var cookieParser = require("cookie-parser");
+var flash = require("connect-flash");
+
+
 
 
 // import routers
 var homeRouter = require('./routers/index');
 
+const app = express();
 
+app.use(session({
+    secret: "47hr3v7hk3i",
+    saveUninitialized: true,
+    resave: true
+
+}))
+app.use(cookieParser("8r5f8s9&dsj"));
+app.use(flash());
+
+// set up view engine.
+app.set("view engine", "ejs");
+
+// middleware update flashMessage var in view to req.flash().
+app.use('/', (req, res, next) => {
+    res.locals.flashMessages = req.flash();
+    next();
+})
+// routing.
 app.get('/', homeRouter);
 
 
@@ -13,3 +36,9 @@ app.listen(3000, (err) => {
     if(err) throw err;
     else console.log("Listening on Port 3000");
 });
+a
+
+// should be used only once.
+function uniqueStringGenerator(length) {
+    return Math.random().toString(36).substr(13-length);
+}
